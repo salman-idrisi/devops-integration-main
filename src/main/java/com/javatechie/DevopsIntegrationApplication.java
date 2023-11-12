6f80cfa39164de9a179b42772e4802f2b1551f54
salman-idrisi <144918746+salman-idrisi@users.noreply.github.com>
2023-11-13 00:00:59 +0530

    Update DevopsIntegrationApplication.java

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "welcome to javatechie";
+		return "welcome to devops-integration application";
 	}
 
 	public static void main(String[] args) {
